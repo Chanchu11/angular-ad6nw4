0ccb88b52ef0d3e61e4dc5669a2ec50dcaf8fab8
Chanchu11 <36264805+Chanchu11@users.noreply.github.com>
2022-12-11 22:58:27 +0530

    4 files modified, 7 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,15 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule} from './app-routing.module';
+import { AboutComponent } from './about/about.component';
+import { UserComponent } from './user/user.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule, RouterModule, AppRoutingModule],
+  declarations: [AppComponent, HelloComponent, AboutComponent, UserComponent, HomeComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
